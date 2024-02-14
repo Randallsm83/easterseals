d416d9aabdc4e306d308e413b1c60620d335ed88
Randall Miller <randallsm83@gmail.com>
2024-02-13 23:44:24 -0800

    add continue session option and logging of clicks and final points. add welcome message to user

--- app.js
+++ app.js
@@ -4,17 +4,46 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 8001;
 
-const db = new sqlite3.Database('setup.db', (err) => {
+const db = new sqlite3.Database('es.db', (err) => {
   if (err) {
     throw err;
   } else {
-    console.log('Connected to the setup.db database.');
+    console.log('Connected to the database.');
+
+    db.exec('PRAGMA foreign_keys = ON;', function(error)  {
+    if (error){
+        console.error("===ERROR=== Pragma statement didn't work.");
+    } else {
+        console.log("Foreign Key Enforcement is on.");
+    }
+});
+
     db.serialize(() => {
-      db.run("CREATE TABLE IF NOT EXISTS setup (id INTEGER PRIMARY KEY AUTOINCREMENT, sessionId TEXT, buttonActive TEXT, leftButtonShape TEXT, leftButtonColor TEXT, middleButtonShape TEXT, middleButtonColor TEXT, rightButtonShape TEXT, rightButtonColor TEXT, pointsAwarded INTEGER, clicksNeeded INTEGER, startingPoints INTEGER, sessionLength INTEGER, sessionLengthType TEXT)", [], (err) => {
+      db.run("CREATE TABLE IF NOT EXISTS setup (sessionId TEXT PRIMARY KEY, buttonActive TEXT, leftButtonShape TEXT, leftButtonColor TEXT, middleButtonShape TEXT, middleButtonColor TEXT, rightButtonShape TEXT, rightButtonColor TEXT, pointsAwarded INTEGER, clicksNeeded INTEGER, startingPoints INTEGER, sessionLength INTEGER, sessionLengthType TEXT, continueAfterLimit BOOLEAN)", [], (err) => {
         if (err) {
           throw err.message
         } else {
-          console.log('Table setup is ready.');
+          console.log('Setup table setup is ready.');
+        }
+      });
+    });
+
+    db.serialize(() => {
+      db.run("CREATE TABLE IF NOT EXISTS session (sessionId TEXT, clickedButton TEXT, timestamp DATETIME, FOREIGN KEY (sessionId) REFERENCES setup(sessionId))", [], (err) => {
+        if (err) {
+          throw err.message
+        } else {
+          console.log('Session table setup is ready.');
+        }
+      });
+    });
+
+    db.serialize(() => {
+      db.run("CREATE TABLE IF NOT EXISTS final (sessionId TEXT PRIMARY KEY, finalPoints INTEGER, FOREIGN KEY (sessionId) REFERENCES setup(sessionId))", [], (err) => {
+        if (err) {
+          throw err.message
+        } else {
+          console.log('Final table setup is ready.');
         }
       });
     });
@@ -32,7 +61,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/setup', (req, res) => {
-  const { sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType } = req.body;
+  const { sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType, continueAfterLimit } = req.body;
+
+  const continueAfterLimitBool = continueAfterLimit === 'on';
 
   db.get("SELECT * FROM setup WHERE sessionId = ?", [sessionId], (err, row) => {
     if (err) {
@@ -41,8 +72,8 @@ app.post('/setup', (req, res) => {
     } else if (row) {
       res.status(400).send("Session ID already exists. Please enter a different one.");
     } else {
-      db.run("INSERT INTO setup (sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        [sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType],
+      db.run("INSERT INTO setup (sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType, continueAfterLimit) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        [sessionId, buttonActive, leftButtonShape, leftButtonColor, middleButtonShape, middleButtonColor, rightButtonShape, rightButtonColor, pointsAwarded, clicksNeeded, startingPoints, sessionLength, sessionLengthType, continueAfterLimitBool],
         (err) => {
           if (err) {
             console.error(err.message);
@@ -65,8 +96,60 @@ app.get('/session/:sessionId', (req, res) => {
     } else if (!row) {
       res.status(404).send("Session not found");
     } else {
-      console.log(row)
       res.render('session', { session: row });
+    }
+  });
+});
+
+app.post('/log-click', (req, res) => {
+  const { sessionId, clickedButton } = req.body;
+  const timestamp = new Date().toISOString();
+
+  db.get("SELECT * FROM setup WHERE sessionId = ?", [sessionId], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    } else if (!row) {
+      res.status(404).send("Invalid session, not found");
+    } else {
+      db.run("INSERT INTO session (sessionId, clickedButton, timestamp) VALUES (?, ?, ?)", [sessionId, clickedButton, timestamp], (err) => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          res.send("Logged click");
+        }
+      });
+    }
+  });
+});
+
+app.post('/log-final', (req, res) => {
+  const { sessionId, finalPoints } = req.body;
+
+  db.get("SELECT * FROM setup WHERE sessionId = ?", [sessionId], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error");
+    } else if (!row) {
+      res.status(404).send("Invalid session, not found");
+    }
+  });
+  db.get("SELECT * FROM final WHERE sessionId = ?", [sessionId], (err, row) => {
+    if (err) {
+      console.error(err.message);
+      return res.status(500).send("Internal Server Error");
+    } else if (row) {
+      console.error(`Session ${sessionId} already has points? Final of ${finalPoints} not entered.`);
+      res.status(500).send("Points already exist in the database for this session?");
+    }
+  });
+
+  db.run("INSERT INTO final (sessionId, finalPoints) VALUES (?, ?)", [sessionId, finalPoints], (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`Logged ${finalPoints} points for session ${sessionId}`);
+      res.send(`Logged ${finalPoints} points for session ${sessionId}`);
     }
   });
 });
